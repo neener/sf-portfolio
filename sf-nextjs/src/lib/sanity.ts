8a4f3479dfb5c92f1ac5ab18deee2ac938dee238
neener <janinemrosen@gmail.com>
2024-08-29 13:59:27 -0400

    update client info

--- sf-nextjs/src/lib/sanity.ts
+++ sf-nextjs/src/lib/sanity.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@sanity/client';
 
 const client = createClient({
-  projectId: '6ti8vhaw', 
-  dataset: 'production', 
-  apiVersion: '2023-07-24', 
-  useCdn: false, // `false` if you want to ensure fresh data
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,  // Your Sanity project ID
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,      // Your Sanity dataset (e.g., 'production')
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-07-24', // Use the API version from your .env file or a default value
+  useCdn: true,  // Set to true for faster response times (good for public data)
 });
 
 export default client;
