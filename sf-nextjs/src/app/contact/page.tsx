482738d2fccf1e23768c0502e3ff58ded659cf28
neener <janinemrosen@gmail.com>
2024-10-26 14:28:43 -0400

    adding it raw

--- sf-nextjs/src/app/contact/page.tsx
+++ sf-nextjs/src/app/contact/page.tsx
@@ -3,11 +3,10 @@
 import { useEffect, useState } from 'react';
 import client from '../../lib/sanity';
 import { PortableText } from '@portabletext/react';
-import { urlFor } from '../../lib/sanityImage';
-import Script from 'next/script';
+import { urlFor } from '../../lib/sanityImage'; // Assuming this is your image helper
 
 const ContactPage = () => {
-  const [contact, setContact] = useState<any | null>(null);
+  const [contact, setContact] = useState<any | null>(null); // Store the contact data
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
@@ -42,20 +41,8 @@ const ContactPage = () => {
 
   return (
     <div className="p-4">
-      <Script
-        src="https://code.jquery.com/jquery-3.6.0.min.js"
-        strategy="afterInteractive"
-        onLoad={() => console.log("jQuery loaded successfully")}
-      />
-      <Script
-        src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
-        strategy="afterInteractive"
-      />
-
       <h1 className="text-2xl font-bold">{contact.title}</h1>
-      <p className="text-lg">
-        Email: <a href={`mailto:${contact.email}`} className="text-blue-600 underline">{contact.email}</a>
-      </p>
+      <p className="text-lg">Email: <a href={`mailto:${contact.email}`} className="text-blue-600 underline">{contact.email}</a></p>
       <p className="text-lg">Address: {contact.address}</p>
 
       <div className="mt-6">
@@ -71,33 +58,37 @@ const ContactPage = () => {
           }}
         />
       </div>
+      <div id="mc_embed_shell">
+      <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+        #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 600px;}
+        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+           We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+    <form action="https://gmail.us22.list-manage.com/subscribe/post?u=c51b8d1989d515cd66be11e95&amp;id=02d6493d6c&amp;f_id=00bdd8e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+        <div id="mc_embed_signup_scroll"><h2>Subscribe</h2>
+            <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+            <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
+        <div id="mce-responses" class="clear foot">
+            <div class="response" id="mce-error-response" style="display: none;"></div>
+            <div class="response" id="mce-success-response" style="display: none;"></div>
+        </div>
+    <div aria-hidden="true" style="position: absolute; left: -5000px;">
+        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+        <input type="text" name="b_c51b8d1989d515cd66be11e95_02d6493d6c" tabindex="-1" value="">
+    </div>
+        <div class="optionalParent">
+            <div class="clear foot">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
+                <p style="margin: 0px auto;"><a href="http://eepurl.com/iWyW6E" title="Mailchimp - email marketing made easy and fun"><span style="display: inline-block; background-color: transparent; border-radius: 4px;"><img class="refferal_badge" src="https://digitalasset.intuit.com/render/content/dam/intuit/mc-fe/en_us/images/intuit-mc-rewards-text-dark.svg" alt="Intuit Mailchimp" style="width: 220px; height: 40px; display: flex; padding: 2px 0px; justify-content: center; align-items: center;"></span></a></p>
+            </div>
+        </div>
+    </div>
+</form>
+</div>
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
 
-      <div id="mc_embed_signup" className="mt-8">
-        <h2 className="text-xl font-bold">Subscribe</h2>
-        <form
-          action="https://gmail.us22.list-manage.com/subscribe/post?u=c51b8d1989d515cd66be11e95&amp;id=02d6493d6c&amp;f_id=00bdd8e1f0"
-          method="post"
-          id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form"
-          className="validate"
-          target="_blank"
-        >
-          <div className="mc-field-group">
-            <label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span></label>
-            <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" required />
-          </div>
-          <div id="mce-responses" className="clear foot">
-            <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
-            <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
-          </div>
-          <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
-            <input type="text" name="b_c51b8d1989d515cd66be11e95_02d6493d6c" tabIndex={-1} defaultValue="" />
-          </div>
-          <div className="clear foot">
-            <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Subscribe" />
-          </div>
-        </form>
-      </div>
     </div>
   );
 };
